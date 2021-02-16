657e3f33960012bc1e7004f0c0328d3ca660c62b
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-02-16 11:28:48 +0000

    java ok

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Solutions in Java, Kotlin and Ruby
 
 [![Ruby](https://img.shields.io/badge/Ruby%3F-done-green.svg)](https://github.com/joaogouveia89/hacker-rank-minimum-swaps-2/blob/master/solutions/ruby/minimum_swipes.rb) </br>
-[![Java](https://img.shields.io/badge/Java%3F-doing-yellow.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity) </br>
+[![Java](https://img.shields.io/badge/Java%3F-done-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity) </br>
 [![Kotlin](https://img.shields.io/badge/Kotlin%3F-doing-yellow.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity)
--- solutions/java/MinimumSwipes2.java
+++ solutions/java/MinimumSwipes2.java
@@ -1,3 +1,25 @@
 public class MinimumSwipes2{
+    static int minimumSwaps(int[] arr) {
 
+        int min = 1;
+        int swaps = 0;
+        int supposedPlace;
+        int currentPlace;
+        int temp;
+        
+        for(int i = 0; i < arr.length; i++){
+            supposedPlace = arr[i] - min;
+            if(supposedPlace != i){
+               while(supposedPlace != i){
+                    currentPlace = supposedPlace;
+                    supposedPlace = arr[supposedPlace] - min;
+                    temp = arr[currentPlace];
+                    arr[currentPlace] = arr[i];
+                    arr[i] = temp;
+                    swaps++;
+               }
+            }    
+        }
+        return swaps;
+    }
 }
